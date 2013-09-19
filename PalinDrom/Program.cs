eca46b8f03cb718e9cb7701e12ec5fa5b5ccad7b
Johan Björklund <johan@jsbmaleri.se>
2013-09-19 16:52:28 +0200

    Solved the palindrome task with a boolean and use of System.Linq's command SequenceEqual

--- PalinDrom/Program.cs
+++ PalinDrom/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-
+using System.Linq;
 
 
 class Program
 {
     public static string ReverseText(string text)
     {
-        //string result = "";
+        
+        string original = text;
 
         char[] charArray = text.ToCharArray();
         Array.Reverse(charArray);
+        //string reversed = new string (reversed.Reverse(original.ToCharArray()));
+
         return new string(charArray);
 
 
@@ -17,20 +20,20 @@ class Program
         //return result;
     
     }
-    public static string Palindrom(string i)
+    public static bool Palindrom(string i)
     {
-        return i;
+        return i.SequenceEqual(i.Reverse());
     }
    
 
 
     static void Main()
     {
-        Console.WriteLine("Please enter a word to spell it backwards and to see if it's a palindrome");
+        Console.WriteLine("Please enter a word to spell it backwards and to see if it's a palindrome\n");
         string word = Console.ReadLine();
         word = word.ToLower();
-        Console.WriteLine(ReverseText(word));
-        Console.WriteLine(Palindrom(word));
+        Console.Write("\nHere is your word spelled backwards: \n\n" + ReverseText(word) + "\n");
+        Console.WriteLine("\n Is your word a palindrome? \n\n" + Palindrom(word) + "\n");
         
         
     }
