4659bfc295fffdb705d5e5eaf1da0ec3599be524
Johan Björklund <johan@jsbmaleri.se>
2013-09-19 17:02:18 +0200

    Cleaned it up a bit, got rid of unessecary lines

--- PalinDrom/Program.cs
+++ PalinDrom/Program.cs
@@ -5,27 +5,16 @@ using System.Linq;
 class Program
 {
     public static string ReverseText(string text)
-    {
-        
-        string original = text;
-
+    {        
         char[] charArray = text.ToCharArray();
         Array.Reverse(charArray);
-        //string reversed = new string (reversed.Reverse(original.ToCharArray()));
-
         return new string(charArray);
-
-
-
-        //return result;
-    
     }
+
     public static bool Palindrom(string i)
     {
         return i.SequenceEqual(i.Reverse());
     }
-   
-
 
     static void Main()
     {
@@ -34,8 +23,7 @@ class Program
         word = word.ToLower();
         Console.Write("\nHere is your word spelled backwards: \n\n" + ReverseText(word) + "\n");
         Console.WriteLine("\n Is your word a palindrome? \n\n" + Palindrom(word) + "\n");
-        
-        
+               
     }
 }
 
